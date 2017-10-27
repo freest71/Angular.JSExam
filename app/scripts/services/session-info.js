fae3d3277a27e20660cd0957228d669f39c8501b
yu yunho <freest71@hanmail.net>
2017-10-27 17:19:58 +0900

    토큰로직추가

---
+++ app/scripts/services/session-info.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name angularjsexamApp.sessionInfo
+ * @description
+ * # sessionInfo
+ * Service in the angularjsexamApp.
+ */
+angular.module('angularjsexamApp')
+  .service('sessionInfo', function () {
+    // AngularJS will instantiate a singleton by calling "new" on this function
+  });
---
+++ app/scripts/services/session-service.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name angularjsexamApp.sessionService
+ * @description
+ * # sessionService
+ * Service in the angularjsexamApp.
+ */
+angular.module('angularjsexamApp')
+  .service('sessionService', function () {
+    // AngularJS will instantiate a singleton by calling "new" on this function
+  });
